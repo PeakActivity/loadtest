8cb7d4c984096191271c4e68480b205ef0c4d235
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-11-01 11:20:51 -0400

    comments

--- src/tests/weightedURLTraffic.js
+++ src/tests/weightedURLTraffic.js
@@ -10,8 +10,8 @@ export const options = {
   stages: [
     { duration: '10s', target: 1 }, // below normal load
     { duration: '1m', target: 5 }, // scale up to normal load
-    { duration: '10s', target: 10 }, // spike to 1400 users
-    { duration: '30s', target: 10 }, // stay at 1400 for 3 minutes
+    { duration: '10s', target: 10 }, // spike to 10 users
+    { duration: '30s', target: 10 }, // stay at 10 for 3 minutes
     { duration: '10s', target: 5 }, // scale down. Recovery stage.
     { duration: '5s', target: 1 }, // stay at normal load.
     { duration: '10s', target: 0 }, // scale to zero and end test.
