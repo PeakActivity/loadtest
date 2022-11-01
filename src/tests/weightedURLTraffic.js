326d1509524fd4c68b6fa29162ba8ee30c7b6eaf
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-11-01 11:20:42 -0400

    added comments.

--- src/tests/weightedURLTraffic.js
+++ src/tests/weightedURLTraffic.js
@@ -8,13 +8,13 @@ import http from 'k6/http';
 
 export const options = {
   stages: [
-    // { duration: '10s', target: 1 }, // below normal load
-    // { duration: '1m', target: 100 },
-    // { duration: '10s', target: 1400 }, // spike to 1400 users
-    // { duration: '3m', target: 1400 }, // stay at 1400 for 3 minutes
-    // { duration: '10s', target: 100 }, // scale down. Recovery stage.
-    // { duration: '3m', target: 100 },
-    // { duration: '10s', target: 0 },
+    { duration: '10s', target: 1 }, // below normal load
+    { duration: '1m', target: 5 }, // scale up to normal load
+    { duration: '10s', target: 10 }, // spike to 1400 users
+    { duration: '30s', target: 10 }, // stay at 1400 for 3 minutes
+    { duration: '10s', target: 5 }, // scale down. Recovery stage.
+    { duration: '5s', target: 1 }, // stay at normal load.
+    { duration: '10s', target: 0 }, // scale to zero and end test.
   ],
 };
 
@@ -27,6 +27,7 @@ const data = new SharedArray('urls', function () {
 // Run by Virtual User once per iteration
 export default function () {
   const createRandomURL = () => {
+    // Average Weighted array
     const randomPath = randomItem(data);
     const basePath = 'https://www.shoesforcrews.com';
     return basePath + randomPath;
